351584874ab0c2f06dda06794f71e99772a1fcfc
Killian264 <killiandebacker@gmail.com>
2020-09-17 15:50:52 -0500

    class skeletons

---
+++ CSC470-P3/FakeAppUserRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSC470_P3
+{
+    public class FakeAppUserRepository : IAppUser
+    {
+        private static Dictionary<int, AppUser> Users;
+
+        public FakeAppUserRepository()
+        {
+            if(Users == null)
+            {
+                Users = new Dictionary<int, AppUser>();
+
+                Users.Add(1, new AppUser
+                {
+                    UserName = "John240",
+                    Password = "password123",
+                    FirstName = "John",
+                    LastName = "Doe",
+                    EmailAddress = "JohnDoe@gmail.com",
+                    isAuthenticated = false,
+                });
+            }
+        }
+        bool IAppUser.Login(string UserName, string Password)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IAppUser.SetAuthentication(string UserName, bool IsAuthenticated)
+        {
+            throw new NotImplementedException();
+        }
+
+        AppUser IAppUser.GetByUserName(string UserName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CSC470-P3/IAppUser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSC470_P3
+{
+    public interface IAppUser
+    {
+        bool Login(string UserName, string Password);
+        void SetAuthentication(string UserName, bool IsAuthenticated);
+        AppUser GetByUserName(string UserName);
+    }
+}
